e93d9aa6cef6c02271a4ce9dc4a0564c201abf89
Jacob Critch <jacobc@mun.ca>
2020-02-20 02:30:55 -0330

    Initial push
    - Added rate limiting to backend with Redis.
    
    - Added HTML template.
    
    - Added main.js with most frontend functionality complete to /static (incl. charts, table, map)
    
    -

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 import sqlalchemy
+import redis
+import time
+import sys
 
 # web app
 app = Flask(__name__)
 
 # database engine
 engine = sqlalchemy.create_engine(os.getenv('SQL_URI'))
-
+r = redis.StrictRedis(decode_responses=True)
 
 @app.route('/')
 def index():
-    return 'Welcome to EQ Works 😎'
+    return render_template('index.html')
 
-
-@app.route('/events/hourly')
+@app.route('/events/hourly', methods=['GET', 'POST'])
 def events_hourly():
+    if rateChecker(request) == "False": return "Rate exceeded, try again later"
     return queryHelper('''
         SELECT date, hour, events
         FROM public.hourly_events
@@ -25,9 +28,9 @@ def events_hourly():
         LIMIT 168;
     ''')
 
-
 @app.route('/events/daily')
 def events_daily():
+    if rateChecker(request) == "False": return "Rate exceeded, try again later"
     return queryHelper('''
         SELECT date, SUM(events) AS events
         FROM public.hourly_events
@@ -36,9 +39,9 @@ def events_daily():
         LIMIT 7;
     ''')
 
-
 @app.route('/stats/hourly')
 def stats_hourly():
+    if rateChecker(request) == "False": return "Rate exceeded, try again later"
     return queryHelper('''
         SELECT date, hour, impressions, clicks, revenue
         FROM public.hourly_stats
@@ -46,9 +49,9 @@ def stats_hourly():
         LIMIT 168;
     ''')
 
-
 @app.route('/stats/daily')
 def stats_daily():
+    if rateChecker(request) == "False": return "Rate exceeded, try again later"
     return queryHelper('''
         SELECT date,
             SUM(impressions) AS impressions,
@@ -62,6 +65,7 @@ def stats_daily():
 
 @app.route('/poi')
 def poi():
+    if rateChecker(request) == "False": return "Rate exceeded, try again later"
     return queryHelper('''
         SELECT *
         FROM public.poi;
@@ -71,3 +75,24 @@ def queryHelper(query):
     with engine.connect() as conn:
         result = conn.execute(query).fetchall()
         return jsonify([dict(row.items()) for row in result])
+        
+def rateChecker(req):
+    limit = 20 # Limit of requests per minute
+    ip = req.environ['REMOTE_ADDR']
+    
+    if r.exists(ip):
+        ipDict = r.hgetall(ip) # Get dictionary of attributes from client's IP
+        numReqs = int(ipDict["numReqs"]) 
+
+        if time.time() > float(ipDict["expiry"]): # If expiry time limit has passed set new expiry time
+            r.hmset(ip, { "numReqs": 1, "expiry": str(time.time()+60) })
+            return "True"
+        elif time.time() < float(ipDict["expiry"]): # Else if expiry time not passed yet
+            if numReqs >= limit:
+                return "False"
+            if numReqs <= limit:
+                r.hmset(ip, { "numReqs": int(numReqs+1) })
+                return "True"
+    else:
+        r.hmset(ip, { "numReqs": 1, "expiry": str(time.time() + 60) })
+        return "True"
---
+++ static/main.js
@@ -0,0 +1,195 @@
+bindHeaderButtonEvents()
+createEventsPage()
+
+function bindHeaderButtonEvents() {
+	$(document).ready(() => {
+		$("#eventsPageButton").click((e) => {
+			createEventsPage();
+		})
+
+		$("#tablePageButton").click((e) => {
+			createTablePage();
+		})
+
+		$("#mapPageButton").click((e) => {
+			createMapPage();
+		})
+	})
+}
+
+
+function createEventsPage() {
+	$(document).ready(() => {
+		$("#canvasContainer").html("")
+		$("#canvasContainer").append(
+			'<div class="row" id="eventsRow">\
+				<canvas id="mainCanvas" width="50px" height="50px"></canvas>\
+				<button type="button" class="btn btn-primary" id="eventsDailyButton">Events by day</button>\
+				<button type="button" class="btn btn-primary" id="eventsHourlyButton">Events by hour</button>\
+			</div>'
+		)
+		createEventsDailyChart()
+		$("#eventsDailyButton").click((e) => {
+			createEventsDailyChart()
+		})
+
+		$("#eventsHourlyButton").click((e) => {
+			createEventsHourlyChart()
+		   })
+	})
+}
+
+
+function createEventsDailyChart() {
+	dates = [];
+	events = [];
+	$.get("/events/daily", {}, (data) => {
+		if (data == "Rate exceeded, try again later") {
+			alert(data)
+		}
+		data.forEach( (e, i) => {
+			dates.push(e["date"].slice(0, -13));
+			events.push(e["events"]);
+		})
+	}).then(() => {
+		$("#mainCanvas").remove();
+		$("#canvasContainer").prepend('<canvas id="mainCanvas" width="50px" height="50px"></canvas>')
+		createChart("line", "Events by day", dates, events)
+	})
+}
+
+
+function createEventsHourlyChart() {
+	dates = [];
+	events = [];
+	$.get("/events/hourly", {}, (data) => {
+		if (data == "Rate exceeded, try again later") {
+			alert(data)
+			return
+		}
+		data.forEach( (e, i) => {
+			dates.push(e["date"].slice(0, -13) + " Hour: " + e["hour"])
+			events.push(e["events"])
+		})
+	}).then(() => {
+		$("#mainCanvas").remove();
+		$("#canvasContainer").prepend('<canvas id="mainCanvas" width="50px" height="50px"></canvas>')
+		createChart("line", "Events by hour", dates, events)
+	})
+}
+
+
+function createTablePage() {
+	$(document).ready(() => {
+		$("#canvasContainer").html("")
+		$("#canvasContainer").append(
+			'<div class="row">\
+			<table id="dataTable" class="display" style="width:100%">\
+					<thead>\
+						<tr>\
+							<th>Date</th>\
+							<th>Clicks</th>\
+							<th>Impressions</th>\
+							<th>Revenue</th>\
+						</tr>\
+					</thead>\
+					<tbody>\
+					</tbody>\
+					<tfoot>\
+						<tr>\
+							<th>Date</th>\
+							<th>Clicks</th>\
+							<th>Impressions</th>\
+							<th>Revenue</th>\
+						</tr>\
+					</tfoot>\
+				</table>\
+			</div>'
+		)
+		createDataTable()
+	})
+}
+
+
+function createDataTable() {
+	$.get("/stats/hourly", {}, (data) => {
+		if (data == "Rate exceeded, try again later") {
+			alert(data)
+			return
+		}
+		data.forEach((e,i) => {
+			$("#dataTable").append("<tr><td>"+e["date"].slice(0, -13)+" @ "+e["hour"]+" hour"+"</td><td>"+e["clicks"]+"</td><td>"+e["impressions"]+"</td><td>"+e["revenue"])
+		})
+	}).then(() => {
+		dataTable = $('#dataTable').DataTable()
+		}
+	)
+}
+
+
+function createMapPage() {
+	$(document).ready(() => {
+		$("#canvasContainer").html("")
+		$("#canvasContainer").append('<div id="mapContainer" style="height: 600px;"></div>')
+		$("#canvasContainer").append('<button type="button" class="btn btn-primary" id="poi1Button">EQ Works</button>\
+									  <button type="button" class="btn btn-primary" id="poi2Button">CN Tower</button>\
+									  <button type="button" class="btn btn-primary" id="poi3Button">Niagara Falls</button>\
+									  <button type="button" class="btn btn-primary" id="poi4Button">Vancouver Harbour</button>')
+		createMap()
+	})
+}
+
+
+function createMap() {
+	$(document).ready(() => {
+		var mymap = L.map('mapContainer').setView([51.505, -0.09], 13);	
+		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFjb2Jjcml0Y2giLCJhIjoiY2s2dThmdGdsMDdicDNocG55ZW56ZzdwYiJ9.frUrzAz0nb0AK04KHxtSYQ', {
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			maxZoom: 18,
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1,
+			accessToken: 'pk.eyJ1IjoiamFjb2Jjcml0Y2giLCJhIjoiY2s2dThmdGdsMDdicDNocG55ZW56ZzdwYiJ9.frUrzAz0nb0AK04KHxtSYQ'
+		}).addTo(mymap);
+		$("#poi1Button").click((e) => {
+			mymap.panTo(new L.LatLng(43.6708, -79.3899))
+		})
+		$("#poi2Button").click((e) => {
+			mymap.panTo(new L.LatLng(43.6426, -79.3871))
+		})
+		$("#poi3Button").click((e) => {
+			mymap.panTo(new L.LatLng(43.0896, -79.0849))
+		})
+		$("#poi4Button").click((e) => {
+			mymap.panTo(new L.LatLng(43.6708, -79.3899))
+		})
+
+	})
+}
+
+
+function createChart(typeStr, labelStr, labelsList, dataList) {
+	$(document).ready( () => {
+	var ctx = document.getElementById('mainCanvas').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: typeStr,
+		data: {
+			labels: labelsList,
+			datasets: [{
+				label: labelStr,
+				data: dataList,
+				borderWidth: 1,
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+						}
+					}]
+				}
+			}
+		})
+	})
+}
